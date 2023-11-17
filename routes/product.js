c00a8e7d5e08aa586d941b04ee8f6c441d065293
ilian <ilian.aourou@edu.devinci.fr>
2023-11-17 16:44:13 +0100

    premier commit

---
+++ routes/product.js
@@ -0,0 +1,10 @@
+/**
+ * Création d'un CRUD de gestion des produits
+ * 
+ * Un produit est qualifié par:
+ *  - un nom (name)
+ *  - une description (description) min 50 caractères
+ *  - un prix (unit_price) > 0
+ *  - une quantité en stock (stock_quantity) > 0
+ *  - une catégorie (category) compris entre ['alimentation', 'hygiène', 'entretien', 'autre']
+ */
---
+++ routes/security.js
@@ -0,0 +1,44 @@
+const { Router } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = new Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!user)
+      res.status(422).json({
+        email: "Invalid credentials",
+      });
+    else {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        const token = jwt.sign(
+          {
+            sub: user.id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "90 days",
+          }
+        );
+        res.json({
+          token,
+        });
+      } else
+        res.status(422).json({
+          email: "Invalid credentials",
+        });
+    }
+  } catch (err) {
+    res.status(422).json({
+      email: err.message,
+    });
+  }
+});
+
+module.exports = router;
