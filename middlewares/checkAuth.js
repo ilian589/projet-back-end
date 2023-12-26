b35c5b43d76a60dce82c078b6345cd14a3ac12e1
Madboy229 <95979604+Madboy229@users.noreply.github.com>
2023-12-26 21:40:52 +0100

    Update checkAuth.js

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -2,14 +2,18 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const authHeader = req.headers.Authorization ?? req.headers.authorization;
-  if (!authHeader) res.sendStatus(401);
+  if (!authHeader) return res.sendStatus(401);
+  
   const [type, token] = authHeader.split(/\s+/);
-  if (type !== "Bearer") res.sendStatus(401);
+  if (type !== "Bearer") return res.sendStatus(401);
+  
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = payload.sub;
     next();
-  } catch (e) {
+  } 
+  catch (e) 
+  {
     res.sendStatus(401);
     console.error(e);
   }
